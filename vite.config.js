d3f82a1c280f9230ac1f0576cbcd869085c49109
tolia k <tolia.kohan@gmail.com>
2025-10-15 08:39:00 +0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-});
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+        },
+    },
+})
